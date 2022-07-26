c46a83af8f8724c2e07c17761daa6786b6ed0834
Lofty | Brambles <prantikdas2004@gmail.com>
2022-07-26 17:07:16 +0530

    📄 | Adds Homepage

--- src/pages/Homepg.tsx
+++ src/pages/Homepg.tsx
@@ -1,5 +1,27 @@
 import React from "react";
+import "./Homepg.scss";
+
+import DiscordLogo from "../assets/discord.webp";
+import GithubLogo from "../assets/github.png";
 
 export default function Homepg() {
-	return <div>p</div>;
+	return (
+		<main>
+			<div>
+				<div id="space">
+					<h1>Hungry? Grab a slice!</h1>
+					<button type="button">Click to order!</button>
+				</div>
+				<div id="left">
+					<a href="https://discord.com/users/740094143379800156">
+						<img src={DiscordLogo} alt="Discord" />
+					</a>
+					<p>Reach out to us!</p>
+					<a href="https://github.com/Lofty-Brambles/restuarant-page">
+						<img src={GithubLogo} alt="Github" />
+					</a>
+				</div>
+			</div>
+		</main>
+	);
 }
